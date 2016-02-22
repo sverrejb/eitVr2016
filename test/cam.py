52e40d0db76816cf3fcd8814cdf1996ae80585c3
Sverre Johann Bjørke <sverrejb@gmail.com>
2016-02-22 18:39:39 +0100

    improved fps by image resize

--- test/cam.py
+++ test/cam.py
@@ -1,36 +1,36 @@
 import numpy as np
 import cv2
-import scipy.ndimage
+#import scipy.ndimage
 import threading
 #from rift import PyRift
 
-cam1 = cv2.VideoCapture(0)
-cam2 = cv2.VideoCapture(1)
-
-## scaling ###
-cam1.set(3,1920)
-cam1.set(4,1080)
-
-cam2.set(3,1920)
-cam2.set(4,1080)
 
 class Buffer(object):
     def __init__(self):
-        ret, self.right = cam1.read() 
+        ret, self.right = cam1.read()
         ret, self.left = cam2.read()
 
-cv2.namedWindow('frame',cv2.WND_PROP_FULLSCREEN)
-
-buffer = Buffer()
 
 def update_left(buffer):
     while True:
-        ret, buffer.left = cam1.read()
+        ret, frame = cam1.read()
+        frame = frame[0:1080, 480:1440] # Crop from x, y, w, h -> 100, 200, 300, 400
+        buffer.left = frame
 
 
 def update_right(buffer):
     while True:
-        ret, buffer.right = cam2.read()
+        ret, frame = cam2.read()
+        frame = frame[0:1080, 480:1440] # Crop from x, y, w, h -> 100, 200, 300, 400
+        buffer.right = frame
+
+
+cam1 = cv2.VideoCapture(0)
+cam2 = cv2.VideoCapture(1)
+
+cv2.namedWindow('frame', cv2.WND_PROP_FULLSCREEN)
+
+buffer = Buffer()
 
 left_thread = threading.Thread(target = update_left, args = (buffer,))
 right_thread = threading.Thread(target = update_right, args = (buffer,))
@@ -38,12 +38,11 @@ right_thread = threading.Thread(target = update_right, args = (buffer,))
 left_thread.start()
 right_thread.start()
 
-while(True):
+while True:
 
-    #frame1 = frame1[0:1080, 480:1440] # Crop from x, y, w, h -> 100, 200, 300, 400
-    #frame2 = frame2[0:1080, 480:1440] # Crop from x, y, w, h -> 100, 200, 300, 400
+    frame = np.concatenate((buffer.left, buffer.right), axis=1)
 
-    frame = np.concatenate((buffer.right, buffer.left), axis=1)
+    frame = cv2.resize(frame,(1920, 1080), interpolation = cv2.INTER_CUBIC)
 
     # Display the resulting frame
     cv2.imshow('frame', frame)
