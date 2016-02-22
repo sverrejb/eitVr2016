203b9a4e10954fe2c810b95ac1b2a18a46ea1a60
Sverre Johann Bjørke <sverrejb@gmail.com>
2016-02-22 11:12:49 +0100

    dual camera feed sort of working

--- test/cam.py
+++ test/cam.py
@@ -1,28 +1,42 @@
 import numpy as np
 import cv2
 import scipy.ndimage
+#from rift import PyRift
 
-cam1 = cv2.VideoCapture(0)
+cam1 = cv2.VideoCapture(1)
 cam2 = cv2.VideoCapture(1)
+
+cam1.set(3,1920)
+cam1.set(4,1080)
+
+cam2.set(3,1920)
+cam2.set(4,1080)
+
+cv2.namedWindow('frame',cv2.WND_PROP_FULLSCREEN)
 
 while(True):
     # Capture frame-by-frame
+    # to threads?
     ret1, frame1 = cam1.read()
-    ret2, frame2 = cam2.read()
-
-    frame1 = cv2.resize(frame1, (0,0), fx=0.5, fy=0.5) 
-    frame2 = cv2.resize(frame2, (0,0), fx=0.5, fy=0.5) 
+    ret2, frame2 = cam1.read()
 
     # Our operations on the frame come here
-    vis = np.concatenate((frame1, frame2), axis=1)
 
+    frame1 = frame1[0:1080, 480:1440] # Crop from x, y, w, h -> 100, 200, 300, 400
+    frame2 = frame2[0:1080, 480:1440] # Crop from x, y, w, h -> 100, 200, 300, 400
+
+    frame = np.concatenate((frame1, frame2), axis=1)
+
+    
+    #frame = cv2.resize(frame, (0,0), fx=1.5, fy=1.5)
+    #frame = cv2.resize(frame,(1920, 1080), interpolation = cv2.INTER_CUBIC)
 
     # Display the resulting frame
-    cv2.imshow('frame', vis)
+    cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 # When everything done, release the capture
-cap.release()
-cap1.release()
+cam1.release()
+cam2.release()
 cv2.destroyAllWindows()
--- test/test.py
+++ test/test.py
@@ -2,23 +2,30 @@ import numpy as np
 import cv2
 import scipy.ndimage
 
-cam1 = cv2.VideoCapture(0)
-cam2 = cv2.VideoCapture(1)
+cam1 = cv2.VideoCapture(1)
+
+cam1.set(3,320)
+cam1.set(4,120)
+
 
 while(True):
     # Capture frame-by-frame
     ret1, frame1 = cam1.read()
-    ret2, frame2 = cam2.read()
 
-    frame1 = cv2.resize(frame1, (0,0), fx=0.5, fy=0.5) 
-    frame2 = cv2.resize(frame2, (0,0), fx=0.5, fy=0.5) 
+    print frame1.shape[:2]
+
+
+    #frame1 = cv2.resize(frame1, (0,0), fx=0.5, fy=0.5) 
 
     # Our operations on the frame come here
-    vis = np.concatenate((frame1, frame2), axis=1)
 
+    cv2.namedWindow('frame', cv2.WND_PROP_FULLSCREEN)          
+    #cv2.setWindowProperty('frame', cv2.WND_PROP_FULLSCREEN, cv2.cv.CV_WINDOW_FULLSCREEN)
+
+    #frame1 = cv2.resize(frame1,(1920, 1080), interpolation = cv2.INTER_CUBIC)
 
     # Display the resulting frame
-    cv2.imshow('frame', vis)
+    cv2.imshow('frame', frame1)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
