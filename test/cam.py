40f57a82f0337603e8e3241cf14e24062ac04205
Sverre Johann Bjørke <sverrejb@gmail.com>
2016-02-23 10:16:46 +0100

    pep8 cleanup

--- test/cam.py
+++ test/cam.py
@@ -1,8 +1,7 @@
 import numpy as np
 import cv2
-#import scipy.ndimage
 import threading
-#from rift import PyRift
+# from rift import PyRift
 
 
 class Buffer(object):
@@ -13,16 +12,17 @@ class Buffer(object):
 
 def update_left(buffer):
     while True:
-        ret, frame = cam1.read()
-        frame = frame[0:1080, 480:1440] # Crop from x, y, w, h -> 100, 200, 300, 400
-        buffer.left = frame
+        ret, left_frame = cam1.read()
+        left_frame = left_frame[0:1080, 480:1440]
+        buffer.left = left_frame
 
 
 def update_right(buffer):
     while True:
-        ret, frame = cam2.read()
-        frame = frame[0:1080, 480:1440] # Crop from x, y, w, h -> 100, 200, 300, 400
-        buffer.right = frame
+        ret, right_frame = cam2.read()
+        # Crop from x, y, w, h -> 100, 200, 300, 400
+        right_frame = right_frame[0:1080, 480:1440]
+        buffer.right = right_frame
 
 
 cam1 = cv2.VideoCapture(0)
@@ -32,17 +32,15 @@ cv2.namedWindow('frame', cv2.WND_PROP_FULLSCREEN)
 
 buffer = Buffer()
 
-left_thread = threading.Thread(target = update_left, args = (buffer,))
-right_thread = threading.Thread(target = update_right, args = (buffer,))
+left_thread = threading.Thread(target=update_left, args=(buffer,))
+right_thread = threading.Thread(target=update_right, args=(buffer,))
 
 left_thread.start()
 right_thread.start()
 
 while True:
-
     frame = np.concatenate((buffer.left, buffer.right), axis=1)
-
-    frame = cv2.resize(frame,(1920, 1080), interpolation = cv2.INTER_CUBIC)
+    frame = cv2.resize(frame, (1920, 1080), interpolation=cv2.INTER_CUBIC)
 
     # Display the resulting frame
     cv2.imshow('frame', frame)
