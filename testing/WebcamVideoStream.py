f68a12bbdc5614e5676609776f2ec5be09c5e13b
Sverre Johann Bjørke <sverrejb@gmail.com>
2016-02-23 16:49:58 +0100

    starting on tutorial for improved threading

---
+++ testing/WebcamVideoStream.py
@@ -0,0 +1,27 @@
+from threading import Thread
+import cv2
+__author__ = 'sverre'
+
+
+class WebcamVideoStream:
+    def __init__(self, src=0):
+        # initialize the video camera stream and read the first frame
+        self.stream = cv2.VideoCapture(src)
+        (self.grabbed, self.frame) = self.stream.read()
+        self.stopped = False
+
+    def start(self):
+        Thread(target=self.update, args=()).start()
+        return self
+
+    def update(self):
+        while True:
+            if self.stopped:
+                return
+            (self.grabbed, self.frame) = self.stream.read()
+
+    def read(self):
+        return self.frame
+
+    def stop(self):
+        self.stopped = True
